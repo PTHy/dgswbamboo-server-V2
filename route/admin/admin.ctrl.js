93c051aa7d5b1957883a10dc126013191f3ef886
seoji <idw5780@dgsw.hs.kr>
2018-11-25 15:33:48 +0900

    [ADD] GET /admin/count/:type
    count function

--- route/admin/admin.ctrl.js
+++ route/admin/admin.ctrl.js
@@ -2,6 +2,47 @@ import rejectPost from '../../database/models/rejectPost';
 import waitPost from '../../database/models/waitPost';
 import allowPost from '../../database/models/allowPost';
 
+exports.count = async (req, res) => {
+  const {
+    type,
+  } = req.params;
+  try {
+    switch (type) {
+      case '0':
+        res.status(200).json({
+          stats: 200,
+          count: await waitPost.find({}).count(),
+        });
+        break;
+      case '1':
+        res.status(200).json({
+          stats: 200,
+          count: await rejectPost.find({}).count(),
+        });
+        break;
+      case '2':
+        res.status(200).json({
+          stats: 200,
+          count: await allowPost.find({}).count(),
+        });
+        break;
+      default:
+        res.status(200).json({
+          stats: 401,
+          desc: '타입이 잘못됐어요',
+        });
+        break;
+    }
+  } catch (error) {
+    const result = {
+      status: 500,
+      error: error.message,
+    };
+    console.log(error.message);
+    res.status(200).json(result);
+  }
+};
+
 exports.reject = async (req, res) => {
   const {
     idx,
--- route/admin/index.js
+++ route/admin/index.js
@@ -3,5 +3,6 @@ const adminCtrl = require('./admin.ctrl');
 
 router.route('/reject').post(adminCtrl.reject);
 router.route('/allow').post(adminCtrl.allow);
+router.route('/count/:type').get(adminCtrl.count);
 
 module.exports = router;
