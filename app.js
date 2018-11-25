dda2abc323c47b43fb0a072dd81ff7431b63bfef
seoji <idw5780@dgsw.hs.kr>
2018-11-25 11:53:34 +0900

    write the App.js
    started

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+import db from './database';
+const {
+    PORT
+} = require('./config.json');
+const app = express();
+const server = http.createServer(app);
+
+app.use(morgan('dev'));
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
+
+app.use(bodyParser.json());
+
+app.use('/',require('./route'));
+
+server.listen(PORT, () => {
+    db();
+    console.log('server runing');
+});
