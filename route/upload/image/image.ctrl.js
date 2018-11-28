64e50a9776b51e97dca1b3544d5171a918851ee0
seoji <idw5780@dgsw.hs.kr>
2018-11-28 10:49:24 +0900

    [FIX] POST /upload/image AND
    models fixed

--- route/upload/image/image.ctrl.js
+++ route/upload/image/image.ctrl.js
@@ -3,22 +3,22 @@ const path = require('path');
 
 exports.UploadImg = async (req, res) => {
   console.log(req.files);
-  const file = [];
-  const reFile = [];
+  const imgs = [];
+  const tumb = [];
   req.files.map((e) => {
-    file.push(e.filename);
+    imgs.push(e.filename);
     const filePath = path.join('public', e.filename);
     gm(filePath).resize(480, 480, '!')
       .write(`public/480_480_${e.filename}`, (err) => {
         if (err) console.log(err);
       });
-    reFile.push(`480_480_${e.filename}`);
+    tumb.push(`480_480_${e.filename}`);
   });
   const result = {
     status: 200,
     desc: 'success',
-    original_files: file,
-    resize_files: reFile,
+    imgs,
+    tumb,
   };
   return res.status(200).json(result);
 };
