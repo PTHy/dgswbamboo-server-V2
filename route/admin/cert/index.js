9ae11a419461d57d52e5ec4fb98e64cc7b21d8eb
seoji <idw5780@dgsw.hs.kr>
2018-11-30 12:19:38 +0900

    [FIX] POST /admin/cert/signup
    어드민 등록은 어드민만 가능합니다

--- route/admin/cert/index.js
+++ route/admin/cert/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const controller = require('./cert.ctrl');
+const certFunc = require('../../../middleware/cert');
 
-router.route('/signup').post(controller.signup);
+router.route('/signup').post(certFunc, controller.signup);
 router.route('/signin').post(controller.signin);
 
 module.exports = router;
